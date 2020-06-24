f898d00df989fea0b7b46302ef14dad1025fa748
danilo patrucco <danilo.patrucco@gmail.com>
2020-06-24 15:09:02 -0400

    Added Log manager and Nlog

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SchoolAPI.Extensions;
+using NLog;
+using System.IO;
 
 namespace SchoolAPI_2
 {
@@ -17,6 +20,7 @@ namespace SchoolAPI_2
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -25,6 +29,7 @@ namespace SchoolAPI_2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureLoggerService();
             services.AddControllers();
         }
 
